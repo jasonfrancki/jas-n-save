5ac814defd5521a0cadcc9b58c8c3fccc09551ac
Jason Francki <jasonfrancki@gmail.com>
2023-01-08 14:08:14 -0600

    added mui Buttons

--- src/Items.js
+++ src/Items.js
@@ -3,6 +3,7 @@ import CssBaseline from '@mui/material/CssBaseline'
 import GlobalStyles from '@mui/material/GlobalStyles'
 import {
   Box,
+  Button,
   Card,
   CardContent,
   CardHeader,
@@ -10,12 +11,12 @@ import {
   Container,
   Snackbar,
   SnackbarContent,
-  Button,
   IconButton,
   Typography,
   Alert,
 } from '@mui/material'
 import './Items.css'
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart'
 
 const Items = ({
   results,
@@ -88,12 +89,15 @@ const Items = ({
                           // variant='subtitle1'
                           align='center'
                         >
-                          <button
-                            className='add-button'
+                          <Button
+                            // className='add-button'
+                            variant='contained'
+                            size='large'
+                            sx={{ width: '100%' }}
                             onClick={() => addToCart(item._id)}
                           >
-                            Add to cart
-                          </button>
+                            <AddShoppingCartIcon />
+                          </Button>
                         </Typography>
                       </ul>
                     </Box>
@@ -111,11 +115,10 @@ const Items = ({
         anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
       >
         <Alert
-          sx={{ width: '100%' }}
+          variant='filled'
+          sx={{ width: '100%', fontSize: '1.5rem' }}
           severity={
-            cartMessage.toLowerCase() === 'added to cart'
-              ? 'success'
-              : 'warning'
+            cartMessage.toLowerCase() === 'added to cart' ? 'success' : 'error'
           }
         >
           <span id='client-snackbar'>{cartMessage}</span>
